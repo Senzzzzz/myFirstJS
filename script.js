0a07113bf95f34aeed1368d242be8c3207375cad
Senzzzzz <senzhao123@gmail.com>
2025-10-04 13:38:35 +0800

    new project

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-
-const firstName = "sen"
-const lastName = "zhao"
-
-function greeting() {
-  console.log('hi there, ' + firstName + lastName)
+let printableAscii = [];
+for (let i = 32; i <= 126; i++) {
+  printableAscii.push(String.fromCharCode(i));
 }
 
-greeting()
+
+function passWordGeneration() {
+  const passWord = []
+  for (i = 0; i < printableAscii.length; i++) {
+    const randomPassword = Math.floor(Math.random() * printableAscii.length)
+    const generatedPassword = printableAscii[randomPassword]
+    passWord.push(generatedPassword)
+  }
+  return passWord.join("")
+}
+
+
+console.log(passWordGeneration())
