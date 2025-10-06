e64bc007d87713111a34af283915c43da0d4aeb6
Senzzzzz <senzhao123@gmail.com>
2025-10-06 21:58:48 +0800

    made changes

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
+const saveInputBtn = document.getElementById("input-btn")
 const list = document.getElementById('ul-el')
+const saveTabBtn = document.getElementById('tab-btn')
+const delAllBtn = document.getElementById('del-btn')
 
 
-inputBtn.addEventListener("click", function () {
+const storedValue = JSON.parse(localStorage.getItem('myLeads'))
+
+if (storedValue) {
+  myLeads = storedValue
+  render(myLeads)
+}
+// Event Listeners
+saveInputBtn.addEventListener("click", ()  => {
   myLeads.push(inputEl.value)
-  render()
-  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+  render(myLeads) 
 })
 
-function render(value) {
+const tempURL = [
+  {url: "www.google.com"}
+]
+
+saveTabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+    render(myLeads)
+});
+
+})
+
+delAllBtn.addEventListener("dblclick", () => {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+function render(array) {
   // list.innerHTML = myLeads.map((lead) => `<li>${lead}</li>`).join("")
 
   // list.innerHTML = ""
@@ -19,18 +47,18 @@ function render(value) {
   //     li.innerHTML = myLeads[i]
   //     list.appendChild(li)
   //   }
+  let listItems = ""
 
-  listItems = `
+  for (let i = 0; i < array.length; i++) {
+    listItems += `
     <li>
         <a target='_blank' href="">
-          ${inputEl.value}
+          ${array[i]}
         </a>
     </li>`
-  list.innerHTML += listItems
- 
+  }
+  
+  list.innerHTML = listItems
 }
 
 
-
-const string = Number("1234")
-console.log(typeof(string))
