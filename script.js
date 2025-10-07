2519efbc61cbcf5edc0f722d22f533012a77f4c2
Senzzzzz <senzhao123@gmail.com>
2025-10-07 19:54:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
-// let myLeads = []
-// const inputEl = document.getElementById("input-el")
-// const saveInputBtn = document.getElementById("input-btn")
-// const list = document.getElementById('ul-el')
-// const saveTabBtn = document.getElementById('tab-btn')
-// const delAllBtn = document.getElementById('del-btn')
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const saveInputBtn = document.getElementById("input-btn")
+const list = document.getElementById('ul-el')
+const saveTabBtn = document.getElementById('tab-btn')
+const delAllBtn = document.getElementById('del-btn')
 
 
-// const storedValue = JSON.parse(localStorage.getItem('myLeads'))
+const storedValue = JSON.parse(localStorage.getItem('myLeads'))
 
-// if (storedValue) {
-//   myLeads = storedValue
-//   render(myLeads)
-// }
-// // Event Listeners
-// saveInputBtn.addEventListener("click", ()  => {
-//   myLeads.push(inputEl.value)
-//   localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-//   render(myLeads) 
-// })
+if (storedValue) {
+  myLeads = storedValue
+  render(myLeads)
+}
+// Event Listeners
+saveInputBtn.addEventListener("click", ()  => {
+  myLeads.push(inputEl.value)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+  render(myLeads) 
+})
 
-// saveTabBtn.addEventListener("click", () => {
-//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     myLeads.push(tabs[0].url)
-//     localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-//     render(myLeads)
-// });
+saveTabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+    render(myLeads)
+});
 
-// })
+})
 
-// delAllBtn.addEventListener("dblclick", () => {
-//   localStorage.clear()
-//   myLeads = []
-//   render(myLeads)
-// })
+delAllBtn.addEventListener("dblclick", () => {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
 
-// function render(array) {
-//   // list.innerHTML = myLeads.map((lead) => `<li>${lead}</li>`).join("")
+function render(array) {
+  // list.innerHTML = myLeads.map((lead) => `<li>${lead}</li>`).join("")
 
-//   // list.innerHTML = ""
-//   // for (i = 0; i < myLeads.length; i++) {
-//   //     const li =  document.createElement('li')
-//   //     li.innerHTML = myLeads[i]
-//   //     list.appendChild(li)
-//   //   }
-//   let listItems = ""
+  // list.innerHTML = ""
+  // for (i = 0; i < myLeads.length; i++) {
+  //     const li =  document.createElement('li')
+  //     li.innerHTML = myLeads[i]
+  //     list.appendChild(li)
+  //   }
+  let listItems = ""
 
-//   for (let i = 0; i < array.length; i++) {
-//     listItems += `
-//     <li>
-//         <a target='_blank' href="">
-//           ${array[i]}
-//         </a>
-//     </li>`
-//   }
+  for (let i = 0; i < array.length; i++) {
+    listItems += `
+    <li>
+        <a target='_blank' href="">
+          ${array[i]}
+        </a>
+    </li>`
+  }
   
-//   list.innerHTML = listItems
-// }
-
-
-
-const asdsa = 12334534.34234
-console.log(asdsa.toFixed(2))
+  list.innerHTML = listItems
+}
